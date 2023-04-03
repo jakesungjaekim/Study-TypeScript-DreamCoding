08127160071305c91a32a2ee6d75e0983b032c3f
jakesungjaekim <codejakecarry@gmail.com>
2023-04-03 23:03:35 +0900

    Add: Aliaes

---
+++ 1-types/1-types-practice/1-4-alias.ts
@@ -0,0 +1,26 @@
+{
+  // Type Aliases
+
+  type Text = string;
+
+  const name: Text = 'Jake';
+  const address: Text = 'Korea';
+
+  type Num = number;
+  type Student = {
+    name: string;
+    age: number;
+  };
+  const student: Student = {
+    name: 'Jake',
+    age: 12,
+  };
+
+  // String Literal Types
+
+  type Name = 'name';
+  let JakeName: Name;
+  JakeName = 'name';
+  
+
+} 
